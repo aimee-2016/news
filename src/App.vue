a53d6e2a86671545b8dc1aaad35f3ef80ac62152
lhj <2420479720@qq.com>
2020-06-10 22:04:42 +0800

    lhj

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">资讯</router-link>|
+      <router-link to="/about">视频</router-link>
+      <router-link to="/" class="add">Home</router-link>|
+      <router-link to="/about">话题</router-link>
+      <router-link to="/">我的</router-link>|
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -28,5 +31,13 @@
       color: #42b983;
     }
   }
+}
+.add {
+  width: 40px;
+  height: 40px;
+  background-size: 40px 40px;
+  background-repeat: no-repeat;
+  // @include border-1px(green);
+  @include bg-image("./assets/img/形状55");
 }
 </style>
