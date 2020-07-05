5a9d1428f5d9291f12134094fbb85d115a149d38
eCheo <e1511404018@qq.com>
2020-07-05 23:58:31 +0800

    视频

---
+++ src/components/video/video.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="jz-vbox">
+    <div class="vd-title">
+      <p>标题</p>
+    </div>
+    <video id="video" class="jz-video">
+      <source src="../../assets/img/video/视频.mp4" />您的浏览器不支持 video 标签。
+    </video>
+    <i class="vd-play" @click="player" v-if="pyImg">
+        <img src='../../assets/img/video/play2x.png' />
+      </i>
+    <div class="vd-info">
+        <p>652万次播放量 | 03:58:29</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vd",
+  props: {},
+  data() {
+    return {
+      pyImg: true,
+      flag: false,
+      _dom: ''
+    };
+  },
+  methods: {
+    player() {
+      this._dom = document.getElementById('video');
+      this.pyImg = !this.pyImg;
+      dom.play()
+    },
+    showOtherVideo(){
+      setTimeout(() => {
+          this.flag = this._dom.paused;
+          if(!this.flag){
+              this.showOtherVideo();
+          }
+      },1000)
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.jz-vbox {
+    position: relative;
+    .vd-title {
+        position:absolute;
+        width: 100%;
+        height: 77px;
+        top: 0;
+        left: 0;
+        padding:15px;
+        font-size: 17px;
+        font-weight: bold;
+        color: #fff;
+        z-index: 999;
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5),rgba(255,255,255, 0));
+        box-sizing: border-box;
+    }
+    .vd-info {
+        position:absolute;
+        width: 100%;
+        height: 33px;
+        font-size: 10px;
+        bottom: 2px;
+        left: 0;
+        padding: 12px 15px 12px 0;
+        color: #fff;
+        z-index: 999;
+        background: linear-gradient(to top, rgba(0, 0, 0, 0.5),rgba(255,255,255, 0));
+        box-sizing: border-box;
+        text-align: right;
+    }
+    .vd-play {
+      position: absolute;
+      z-index: 999;
+      top: 56px;
+      left: 42%;
+    }
+  .jz-video {
+    width: 100%;
+    // height: 182px;
+  }
+}
+</style>
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,5 +1,112 @@
 <template>
   <div class="about">
-    <h1>video</h1>
+    <div class="nav">
+      <ul>
+        <li
+          v-for="(item,index) in navList"
+          :key="index"
+          :class="{selected:navId===item.id}"
+          @click="getArticle(item.id)"
+        >{{item.name}}</li>
+      </ul>
+      <van-icon name="search" @click="columnShow=true" />
+    </div>
+    <div class="vd-list">
+      <jz-video />
+    </div>
   </div>
 </template>
+
+<script>
+import { Icon } from "vant";
+import video from "@/components/video/index";
+export default {
+  data() {
+    return {
+      navList: [
+        {
+          value: "关注",
+          name: "关注",
+          id: '1'
+        },
+        {
+          value: "美食",
+          name: "美食",
+          id: '2'
+        }
+      ],
+      navId: '1'
+    };
+  },
+  components: {
+    "van-icon": Icon,
+    "jz-video": video
+    // "van-list": List
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.about {
+  position: relative;
+  padding-top: 10px;
+}
+.search {
+  display: flex;
+  align-items: center;
+  padding: 10px 25px 20px;
+  .van-search {
+    flex: 1;
+    padding: 0;
+  }
+  .van-icon {
+    margin-left: 16px;
+    font-size: 22px;
+  }
+}
+.nav {
+  position: relative;
+  padding: 0 16px;
+  ul {
+    display: flex;
+    align-items: center;
+    padding-right: 20px;
+    li {
+      margin-right: 20px;
+      font-size: 15px;
+      font-family: PingFang SC Medium, PingFang SC Medium-Medium;
+      font-weight: 500;
+      color: #999999;
+      &.selected {
+        position: relative;
+        font-size: 17px;
+        font-family: PingFang SC Heavy, PingFang SC Heavy-Heavy;
+        font-weight: 800;
+        color: #333333;
+        &::after {
+          content: "";
+          display: block;
+          position: absolute;
+          left: 50%;
+          bottom: -6px;
+          transform: translateX(-7.5px);
+          width: 15px;
+          height: 3px;
+          background: #fdd004;
+        }
+      }
+    }
+  }
+  .van-icon-search {
+    position: absolute;
+    top: 0;
+    right: 16px;
+    font-size: 18px;
+    color: #2d2d2d;
+    background: #fff;
+  }
+}
+.vd-list {
+  margin-top: 32px;
+}
+</style>
