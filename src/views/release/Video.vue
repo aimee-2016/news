d11b8e7ecfd1bd04727b632e2f1103ebac867540
lhj <2420479720@qq.com>
2020-11-02 16:51:24 +0800

    video

--- src/views/release/Video.vue
+++ src/views/release/Video.vue
@@ -15,7 +15,7 @@
         placeholder="标题"
       />
       <div class="text-wrap">
-        <div class="wrap van-hairline--bottom">
+        <div class="wrap">
           <van-field
             v-model="content"
             rows="2"
@@ -27,49 +27,63 @@
             :border="false"
             @input="count"
           />
-          <van-uploader
-            v-model="fileList"
-            multiple
-            :after-read="afterRead"
-            :capture="capture1"
-            accept="video/*"
-          >
-            <img
-              class="img1"
-              src="../../assets/img/home/icon-upload@2x.png"
-              alt=""
+          <div class="img-wrap">
+            <van-image
+              width="100%"
+              height="100%"
+              lazy-load
+              fit='cover'
+              :src="imgUrl"
             />
-          </van-uploader>
-
-          <!-- <div class="img-wrap">
-            <label for="image_uploads">换封面</label>
+            <label for="image_uploads">{{imgUrl?'换封面':'上传封面'}}</label>
             <input
               type="file"
               id="image_uploads"
               name="image_uploads"
               accept=".jpg, .jpeg, .png"
-              multiple
+              class="upload" 
+              @change="tirggerFile"
             />
-          </div> -->
+          </div>
         </div>
       </div>
     </div>
-    <van-cell
-      title="选择视频类型"
-      is-link
-      @click="toFiled('ApplyVideoLabel')"
-    />
-    <input type="file" accept="video/*" capture="camcorder" />
-    <input type="file" accept="audio/*" capture="microphone" />
-    <input type="file" accept="image/*" capture="camera" />
+    <div class="type">
+      <div class="wrap">
+        <span class="title">选择视频类型</span>
+        <van-icon name="arrow" v-if="!endZoneName" @click="columnShow=true"/>
+        <span class="text" v-else>{{endZoneName}}</span>
+      </div>
+    </div>
     <div class="bottom">
       <van-button color="#DAB728">发布</van-button>
+    </div>
+    <div class="overlay" v-if="columnShow">
+      <div class="column">
+        <div class="head">
+          <van-icon name="arrow-left" @click="endZoneId = '';endZoneName = '';columnShow=false" />
+          <span> 选择创作领域</span>
+          <i class="right-text" @click="columnShow=false"> 确定</i>
+        </div>
+        <div class="main">
+          <ul>
+            <li
+              v-for="(item, index) in lableList"
+              :key="index"
+              :class="{ selected: item.id === endZoneId }"
+              @click="selectZone(item)"
+            >
+              {{ item.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { Icon, Field, Uploader, Button, Cell } from "vant";
+import { Icon, Field, Uploader, Button, Cell,Image } from "vant";
 export default {
   data() {
     return {
@@ -78,25 +92,43 @@ export default {
       fileList: [],
       title: "",
       content: "",
-      imagePaths: [],
+      imagePath: '',
       contacts: "",
       contactInformation: "",
       email: "",
       capture1: "camcorder",
+      result: '',
+      imgUrl: '',
+      columnShow: false,
+      endZoneId: '',
+      endZoneName: ''
     };
   },
-  created() {},
+  created() {
+    this.getLabelList()
+  },
   mounted() {},
   methods: {
+    tirggerFile(event) {
+      let file = event.target.files[0];
+      let url = "";
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      let that = this;
+      reader.onload = function(e) {
+        url = this.result.substring(this.result.indexOf(",") + 1);
+        that.imgUrl = "data:image/png;base64," + url;
+      };
+      this.afterRead(file)
+    },
     afterRead(file) {
       let formdata = new FormData();
-      formdata.append("filename", file.file.name);
-      formdata.append("file", file.file);
+      formdata.append("filename", file.name);
+      formdata.append("file", file);
       this.$ajax
         .post("api/obs/upload.json", formdata)
         .then((res) => {
-          this.imagePaths.push(res.data.viewUrl);
-          this.$toast(this.imagePaths);
+          this.imagePath=res.data.viewUrl;
         })
         .catch((error) => {
           this.$toast(error);
@@ -104,9 +136,6 @@ export default {
     },
     onOversize(file) {
       this.$toast("文件大小不能超过 500kb");
-    },
-    toFiled(name) {
-      this.$router.push({ path: "/createfiled/", query: { type: name } });
     },
     count(val) {
       this.num = val.length;
@@ -116,30 +145,25 @@ export default {
     },
     submit() {
       if (!(this.title.length > 4 && this.title.length < 31)) {
-        this.$toast("爆料的标题请输入5-30个字）");
+        this.$toast("请输入标题");
         return false;
       }
       if (!this.content.length) {
-        this.$toast("请输入描述");
+        this.$toast("请输入内容");
         return false;
       }
-      if (!this.contacts.length) {
-        this.$toast("请输入联系人");
+      if (!this.content.length) {
+        this.$toast("请选择类型");
         return false;
       }
-      if (!this.contactInformation.length) {
-        this.$toast("请输入联系人方式");
-        return false;
-      }
+     
       this.$ajax
         .post("api/front/articles/release.json", {
           title: this.title,
           content: this.content,
-          type: "BrokeTheNews",
-          imagePaths: this.imagePaths,
-          contacts: this.contacts,
-          contactInformation: this.contactInformation,
-          email: this.email,
+          type: "PublishVideo",
+          videoImagePath: this.imagePath,
+          columnId: this.contacts
         })
         .then(() => {
           this.$toast("提交成功");
@@ -149,6 +173,35 @@ export default {
           this.$toast(error);
         });
     },
+    getLabelList() {
+      this.$ajax
+        .post("api/front/articles/findArticlesColumnListByCondition.json")
+        .then(res => {
+          this.lableList = res.data
+        });
+    },
+    selectZone(item){
+      this.endZoneId = item.id
+      this.endZoneName = item.name
+    },
+    zoneSubmit() {
+      // Dialog.confirm({
+      //     title: `确定要申请${this.endZoneName}领域的标签认证吗？`,
+      //     confirmButtonColor: "rgb(255, 203, 0)",
+      //   })
+      //     .then(() => {
+      //       this.$ajax
+      //       .post("api/front/member/applyLabel.json",{
+      //         applyLabelType: this.$route.query.type,
+      //         columnId: this.endZoneId
+      //       })
+      //       .then(res => {
+      //         this.$router.push('/applystatus/')
+      //       }).catch(error=> {
+      //         this.$toast(error)
+      //       });
+      //     })
+    }
   },
   computed: {},
   watch: {},
@@ -158,6 +211,7 @@ export default {
     "van-field": Field,
     "van-button": Button,
     "van-cell": Cell,
+    "van-image": Image
   },
 };
 </script>
@@ -198,9 +252,11 @@ export default {
 .van-cell {
   background-color: #1b1d29;
   color: #fff;
+  &::after {
+    border-color: #282a37;
+  }
 }
 .top {
-  padding: 0 15px 0;
   .wrap {
     display: flex;
     justify-content: space-between;
@@ -262,6 +318,23 @@ export default {
     }
   }
 }
+.type {
+  padding: 0 15px;
+  .wrap {
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 0;
+    border-top: 1px solid #282a37;
+    border-bottom: 1px solid #282a37;
+  }
+  .title {
+    font-size: 15px;
+    font-family: PingFang SC Medium, PingFang SC Medium-Medium;
+    font-weight: 500;
+    color: #ffffff;
+    line-height: 1;
+  }
+}
 
 .bottom {
   position: fixed;
@@ -278,5 +351,74 @@ export default {
     font-weight: 500;
     color: #ffffff;
   }
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  z-index: 1001;
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  text-align: left;
+  padding-top: 58px;
+  .head {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 100%;
+  padding: 14px 15px;
+  font-size: 17px;
+  font-family: PingFang SC Medium, PingFang SC Medium-Medium;
+  font-weight: 500;
+  text-align: center;
+  color: #333334;
+  line-height: 24px;
+  background: #fff;
+  box-shadow: 0px 1px 7px 0px rgba(0, 0, 0, 0.08);
+  .van-icon-arrow-left {
+    position: absolute;
+    left: 15px;
+    top: 18px;
+    font-size: 17px;
+  }
+  .right-text {
+    font-style: normal;
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    font-size: 15px;
+  }
+}
+.main {
+  padding: 15px 15px 0;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      margin-bottom: 15px;
+      margin-right: 10px;
+      width: 77px;
+      height: 31px;
+      background: #f5f5f5;
+      border-radius: 3px;
+      font-size: 15px;
+      font-family: PingFang SC Medium, PingFang SC Medium-Medium;
+      font-weight: 500;
+      color: #333333;
+      text-align: center;
+      line-height: 31px;
+      &:nth-of-type(4n + 4) {
+        margin-right: 0;
+      }
+      &:hover {
+        background: #fdd004;
+      }
+      &.selected {
+        background: #fdd004;
+      }
+    }
+  }
+}
 }
 </style>
